7dc7019501ed2b94a31281c9e76aaab61c7a2f59
ignacio <ignagazzaniga@gmail.com>
2024-08-15 17:10:04 -0300

    Move methods to RateLimitsService

--- notifications/service.py
+++ notifications/service.py
@@ -1,9 +1,9 @@
 import logging
 import uuid
 from notifications.models import Notification, NotificationType
-from datetime import datetime, timedelta
+from datetime import datetime
 from django.db.utils import IntegrityError
-from rates.service import RateLimitsService
+from rates.service import RateLimitsService, RateLimitError
 from clients.service import ClientsService, ClientDoesNotExistError
 
 logger = logging.getLogger(__name__)
@@ -12,43 +12,6 @@ class IncorrectNotificationTypeError(Exception):
     pass
 
 class NotificationService:
-    def create_notification_type_with_rate(self, name: str, max_times: int, minutes: int):
-        """
-        Create a NotificationType with a given name, max times allowed and minutes.
-        If a NotificationType with the same name exists, raise an IntegrityError
-        If the max_times parameter is not a positive integer, raise an IntegrityError
-        If the minutes parameter is not a positive integer, raise an IntegrityError
-        """
-        try:
-            NotificationType.objects.create(
-                name=name,
-                max_times_allowed=max_times,
-                minutes=minutes
-            )
-        except IntegrityError:
-             logger.error(f'Notification type {name} already exists or some value is incorrect. Please check')
-             raise
-        
-        logger.info(f'Notification type {name} successfully created')
-        return
-
-    def edit_notification_type_rate(self, name: str, max_times: int, minutes: int):
-        """
-        Edit a NotificationType with a given name. 
-        The max times allowed and minutes can be edited.
-        If the max_times parameter is not a positive integer, raise an IntegrityError
-        If the minutes parameter is not a positive integer, raise an IntegrityError
-        """
-        try:
-            NotificationType.objects.filter(name=name).update(max_times_allowed=max_times, minutes=minutes)
-        except IntegrityError:
-            logger.error(f'Notification type {name} update failed because some value is incorrect. Please check')
-            raise
-        else:
-            logger.info(f'Notification type {name} successfully updated')
-
-        return
-
     def send_notification(self, notif_type: str, client_uuid: uuid.UUID, message: str):
         """
         Sends a Notification of a specific type to a Client.
@@ -63,24 +26,21 @@ class NotificationService:
         the provided message and set the datetime as the current datetime.
         """
         try:
+            client = ClientsService().get_client_by_uuid(uuid=client_uuid)
+        except ClientDoesNotExistError:
+            raise
+        
+        try:
             notif_type_obj = NotificationType.objects.get(name=notif_type)
         except NotificationType.DoesNotExist:
             logger.error(f'Notification type {notif_type} does not exist')
             raise IncorrectNotificationTypeError
 
         try:
-            client = ClientsService().get_client_by_uuid(uuid=client_uuid)
-        except ClientDoesNotExistError:
-            raise
-        
-
-        date_to = datetime.now()
-        date_from = date_to - timedelta(minutes=notif_type_obj.minutes)
-        max_times = notif_type_obj.max_times_allowed
-
-        if not RateLimitsService().check_if_rate_is_ok(notif_type, client_uuid, date_from, date_to, max_times):
+            RateLimitsService().check_if_rate_is_ok(notif_type_obj, client_uuid)
+        except RateLimitError:
             logger.warning(f'Notification of type {notif_type} cannot be sent to client {client_uuid} due to rate limits')
-            return
+            raise
         
         Notification.objects.create(
             client=client,
--- rates/service.py
+++ rates/service.py
@@ -1,24 +1,73 @@
+import logging
 import uuid
-from datetime import datetime
-from notifications.models import Notification
+from datetime import datetime, timedelta
+from notifications.models import Notification, NotificationType
+from django.db.utils import IntegrityError
+
+logger = logging.getLogger(__name__)
+
+class RateLimitError(Exception):
+    pass
 
 class RateLimitsService:
+    def create_notification_type_with_rate(self, name: str, max_times: int, minutes: int):
+        """
+        Create a NotificationType with a given name, max times allowed and minutes.
+        If a NotificationType with the same name exists, raise an IntegrityError
+        If the max_times parameter is not a positive integer, raise an IntegrityError
+        If the minutes parameter is not a positive integer, raise an IntegrityError
+        """
+        try:
+            NotificationType.objects.create(
+                name=name,
+                max_times_allowed=max_times,
+                minutes=minutes
+            )
+        except IntegrityError:
+             logger.error(f'Notification type {name} already exists or some value is incorrect. Please check')
+             raise
+        
+        logger.info(f'Notification type {name} successfully created')
+        return
+
+    def edit_notification_type_rate(self, name: str, max_times: int, minutes: int):
+        """
+        Edit a NotificationType with a given name. 
+        The max times allowed and minutes can be edited.
+        If the max_times parameter is not a positive integer, raise an IntegrityError
+        If the minutes parameter is not a positive integer, raise an IntegrityError
+        """
+        try:
+            NotificationType.objects.filter(name=name).update(max_times_allowed=max_times, minutes=minutes)
+        except IntegrityError:
+            logger.error(f'Notification type {name} update failed because some value is incorrect. Please check')
+            raise
+        else:
+            logger.info(f'Notification type {name} successfully updated')
+
+        return
+
     def check_if_rate_is_ok(
             self,
-            notif_type: str,
+            notif_type: object,
             client_uuid: uuid.UUID,
-            date_from: datetime,
-            date_to: datetime,
-            max_times: int
         ):
         """
         Check if a certain notification type can be sent to a user, based on rate limits.
         For this, the Notifications of a certain type between now and the specified minutes must be counted.
-        If the amount is lower, the notification can be sent. Otherwise, deny it.
+        If the amount is lower, the notification can be sent.
+        Otherwise, raise a custom RateLimitError exception.
         """
-        return Notification.objects.filter(
+        date_to = datetime.now()
+        date_from = date_to - timedelta(minutes=notif_type.minutes)
+        max_times = notif_type.max_times_allowed
+
+        if Notification.objects.filter(
             client__uuid=client_uuid,
-            notification_type__name=notif_type,
+            notification_type=notif_type,
             datetime__gte=date_from,
             datetime__lt=date_to
-        ).count() < max_times
+        ).count() >= max_times:
+            raise RateLimitError
+        
+        return True
